302b86ce31ef8c2eea1dc39102143a9d137a9ab0
704484570@qq.com <646866765>
2018-10-16 11:09:46 +0800

    数据绑定

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,60 +1,83 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+import {Layout, Menu, Breadcrumb, Icon} from 'antd';
+import logo from '../../assets/logo.svg';
 
 
+const {SubMenu} = Menu;
+const {Header, Content, Sider} = Layout;
+
+//style={{ alignItems:'center',display: 'flex', padding: ' 0 20px', color: '#fff', fontSize: 20, fontWeight: 'bold' }}
 class App extends Component {
 
-  render() {
-    return (
-      <Layout>
-        <Header >
-          
-        </Header>
-        <Layout>
 
-          <Sider width={225} style={{ background: '#fff' }}>
+    constructor(props) {
+        super(props);
+        this.state = {
+            Breadcrumbs: ['概览']
+        };
+    }
 
-            <Menu mode={"inline"} style={{ height: '100%', borderRight: 0 }}>
+    render() {
 
-              <SubMenu key="sub1" title={<span><Icon type="user" />账户管理</span>}>
-                <Menu.Item key="1">option1</Menu.Item>
-                <Menu.Item key="2">option2</Menu.Item>
-                <Menu.Item key="3">option3</Menu.Item>
-                <Menu.Item key="4">option4</Menu.Item>
-              </SubMenu>
+        return (
+            <Layout>
+                <Header style={{padding: '0 20px'}} className='header'>
+                    <img alt='logo' src={logo} width='34px' height='34px'/>
+                    <span style={{marginLeft: 15}}>华币数字交易所</span>
+                </Header>
 
-              <SubMenu key="sub2" title={<span><Icon type="laptop" />社区</span>}>
-                <Menu.Item key="5">option5</Menu.Item>
-                <Menu.Item key="6">option6</Menu.Item>
-                <Menu.Item key="7">option7</Menu.Item>
-                <Menu.Item key="8">option8</Menu.Item>
-              </SubMenu>
+                <Layout>
+                    <Sider width={225} style={{background: '#fff'}}>
 
-            </Menu>
-          </Sider>
-          
-          <Layout style={{ padding: '0 22px 22px' }}>
-            <Breadcrumb style={{ margin: '14px 0' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
+                        <Menu onClick={this.onMenuSelected.bind(this)} mode={"inline"}
+                              style={{height: '100%', borderRight: 0}}>
+                            <Menu.Item key="概览">
+                                <Icon type="ant-design"/> <span>概览</span>
+                            </Menu.Item>
 
-            <Content style={{ background: '#fff', padding: 22, margin: 0, minHeight: 280 }}>
-              Content
-            </Content>
+                            <SubMenu key="账户管理" title={<span><Icon type="user"/>账户管理</span>}>
+                                <Menu.Item key="账户管理-1">option1</Menu.Item>
+                                <Menu.Item key="账户管理-2">option2</Menu.Item>
+                                <Menu.Item key="账户管理-3">option3</Menu.Item>
+                                <Menu.Item key="账户管理-4">option4</Menu.Item>
+                            </SubMenu>
 
-          </Layout>
-        </Layout>
-      </Layout>
+                            <SubMenu key="社区" title={<span><Icon type="compass"/>社区</span>}>
+                                <Menu.Item key="社区-1">option5</Menu.Item>
+                                <Menu.Item key="社区-2">option6</Menu.Item>
+                                <Menu.Item key="社区-3">option7</Menu.Item>
+                                <Menu.Item key="社区-4">option8</Menu.Item>
+                            </SubMenu>
 
-    );
-  }
+                        </Menu>
+                    </Sider>
+
+                    <Layout style={{padding: '0 22px 22px'}}>
+
+                        <Breadcrumb style={{margin: '14px 0'}}>
+                            {
+                                this.state.Breadcrumbs.reverse().map((value, key) => {
+                                    return <Breadcrumb.Item key={key}> {value}</Breadcrumb.Item>
+                                })
+                            }
+                        </Breadcrumb>
+
+                        <Content style={{background: '#fff', padding: 22, margin: 0, minHeight: 280}}>
+                            Content
+                        </Content>
+
+                    </Layout>
+                </Layout>
+            </Layout>
+
+        );
+    }
+
+    onMenuSelected(o) {
+        this.setState({Breadcrumbs: o.keyPath});
+    }
 }
 
 export default App;
