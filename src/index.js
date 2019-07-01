13335f4bb6704c701412c55a8838c9f61401687b
YellowDoing <704484570@qq.com>
2019-07-01 17:37:06 +0800

    hg

--- src/index.js
+++ src/index.js
@@ -11,5 +11,6 @@ Axios.defaults.baseURL = 'http://139.199.87.52:8080';
 //Axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://139.199.87.52:8080'
 //Axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
 const LoginPage = Form.create()(Login);
-ReactDOM.render(<LoginPage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
